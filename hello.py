2b86ff9d7ee0beccf42e9c793f25534e31f74c8f
Reece Tunesi <rtunesi2000@gmail.com>
2025-02-18 15:23:57 +0000

    Lecture 1 Learning

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
+# Ask user to input their name.
 name = input("What is your name? \n>: ")
-print(f"Hello {name}")
+
+# Remove whitespace from string.
+name = name.strip()
+
+# Capitalise first letter in string.
+name = name.capitalize()
+
+# Capitalise first letter in each word.
+name = name.title()
+
+# Combine the methods
+name = name.strip().title()
+
+# Say hello to the user.
+print(f"Hello \"{name}\"")
