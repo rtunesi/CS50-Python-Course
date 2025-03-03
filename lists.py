2609959d19467246a21e984049e85b3083d34928
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-03 15:52:27 +0000

    Lecture 3 Notes

---
+++ lists.py
@@ -0,0 +1,31 @@
+racers = ["Jack", "Mat"]
+
+def main():
+    global racers
+    print("Welcome to the racers program.")
+    print("Current listed racers:")
+    printRacers()
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    user_count = int(input("How many racers do you want to add?: "))
+    for i in range(user_count):
+        user_count -= 1
+        new_racer = str(input("What is the racers name: "))
+        addRacer(new_racer)
+        print("New racer added successfully... ")
+    print("\nThe racers have been amended, the current listed racers:")
+    printRacers()
+
+
+def addRacer(name):
+    global racers
+    racers.append(name)
+    return racers
+
+
+def printRacers():
+    global racers
+    for person in racers:
+        print(person)
+
+
+main()
