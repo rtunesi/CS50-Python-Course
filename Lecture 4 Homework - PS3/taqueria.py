7d1294a3e44eaa381195cb56bb740a1e49de0361
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-11 12:30:33 +0000

    PS4 - Taqueria.py Start

---
+++ Lecture 4 Homework - PS3/taqueria.py
@@ -0,0 +1,13 @@
+menu = {
+    "BAJA TACO": 4.25,
+    "BURRITO": 7.50,
+    "BOWL": 8.50, 
+    "NACHOS": 11.00,
+    "QUESADILLA": 8.50,
+    "SUPER BURRITO": 8.50,
+    "SUPER QUESADILLA": 9.50, 
+    "TACO": 3.00,
+    "TORTILLA SALAD": 8.00
+}
+
+total = 0
