ad51289d5a7f2f20559dcd5e7952ea1513f936e9
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-11 16:14:37 +0000

    PS4 - Grocery.py amendment

--- Lecture 4 Homework - PS3/grocery.py
+++ Lecture 4 Homework - PS3/grocery.py
@@ -1,19 +1,25 @@
 grocery = {}
-while True:
-    item = str(input()).upper()
 
-    try:
-        if item == "":
+def main():
+    while True:
+        item = str(input()).upper()
+
+        try:
+            if item == "":
+                break
+            elif not item in grocery:
+                grocery.update({item: 1})
+                print(grocery[item])
+            else:
+                grocery[item] += 1
+                print(grocery[item])
+        except EOFError:
             break
-        elif not item in grocery:
-            grocery.update({item: 1})
-            print(grocery[item])
-        else:
-            grocery[item] += 1
-            print(grocery[item])
-    except EOFError:
-        break
 
-print("Final Grocery List")
-for key, value in grocery.items():
-    print(value, key)
+    print("Final Grocery List")
+    for key, value in grocery.items():
+        print(value, key)
+
+
+if __name__ == "__main__":
+    main()
