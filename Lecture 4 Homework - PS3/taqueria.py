b4f89387a4d6a355dca73656e0f2f8f779e4bdac
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-18 14:02:57 +0000

    PS4 - Taqueria.py completion

--- Lecture 4 Homework - PS3/taqueria.py
+++ Lecture 4 Homework - PS3/taqueria.py
@@ -1,11 +1,11 @@
 menu = {
     "BAJA TACO": 4.25,
     "BURRITO": 7.50,
-    "BOWL": 8.50, 
+    "BOWL": 8.50,
     "NACHOS": 11.00,
     "QUESADILLA": 8.50,
     "SUPER BURRITO": 8.50,
-    "SUPER QUESADILLA": 9.50, 
+    "SUPER QUESADILLA": 9.50,
     "TACO": 3.00,
     "TORTILLA SALAD": 8.00
 }
@@ -13,11 +13,12 @@ menu = {
 total = 0
 
 while True:
-    item = str(input("Item: ")).upper()
     try:
+        item = str(input("Item: ")).upper()
         if item in menu:
             total += menu[item]
-            print(f"Total: ${total}")
+            print(f"Total: ${total:.2f}")
     except EOFError:
+        print()
         break
 
