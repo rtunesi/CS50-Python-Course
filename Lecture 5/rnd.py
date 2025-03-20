aba83491161be8d182954d072597bcd9fdb99271
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-20 15:05:08 +0000

    Experimenting with libraries

--- Lecture 5/rnd.py
+++ Lecture 5/rnd.py
@@ -1,4 +1,16 @@
 import random as rd
 
-coin = rd.choice(["Head", "Tails"])
-print(coin)
+count = 0
+head_count = 0
+tail_count = 0
+while count != 100:
+    coin = rd.choice(["Head", "Tails"])
+    print(coin)
+    if coin == "Tails":
+        tail_count += 1
+    else:
+        head_count +=1
+    count += 1
+
+print(head_count)
+print(tail_count)
